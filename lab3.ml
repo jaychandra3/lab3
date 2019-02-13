68b8169521719145584a108b3f85b27d0b6d337a
Jay Chandra <JayChandra@dhcp-10-250-137-172.harvard.edu>
2019-02-13 02:00:01 -0500

    1

--- lab3.ml
+++ lab3.ml
@@ -210,8 +210,10 @@ worry about explicitly handling the anomalous case when the two lists
 are of different lengths.)
 ......................................................................*)
 
-let rec zip (x : 'a list) (y : 'a list) : ('a * 'a) list =
-  failwith "interesting"
+let rec zip (x : int list) (y : int list) : (int * int) list =
+  match x, y with
+  | [], [] -> []
+  | xhd :: xtl, yhd :: ytl -> (xhd, yhd) :: (zip xtl ytl) ;;
 
 (*......................................................................
 Exercise 11: Partitioning a list -- Given a boolean function, say
